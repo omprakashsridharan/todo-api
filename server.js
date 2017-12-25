e5be0c4b32e8642de5c5960915cb9215bcdf6873
omprakash <omprakashsridharan@outlook.com>
2017-12-25 14:22:25 +0530

    Github commit

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var express = require('express'),
     bodyParser = require('body-parser')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://omprakashsridharan:12qwmn09@ds163796.mlab.com:63796/todo-api-db')
-
+//mongoose.connect('mongodb://omprakashsridharan:12qwmn09@ds163796.mlab.com:63796/todo-api-db')
+mongoose.connect('mongodb://localhost:27017/tododb')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
